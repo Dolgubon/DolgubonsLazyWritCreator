dd966693690f8e25abeb55e75fd7bb25076ca5b2
Dolgubon <canopus1000@gmail.com>
2020-02-23 18:39:31 -0500

    Made scroll list have a column for each crafting skill

--- StatsWindow.lua
+++ StatsWindow.lua
@@ -35,33 +35,70 @@ end
 
 function RewardsScroll:SetupEntry(control, data)
 	control.data = data
-
-	local BG = GetControl(control, "BG")
-	local name = GetControl(control, "Name")
-	local amount = GetControl(control, "Amount")
-	name:SetColor(0.4627,0.73725,0.7647)
-	if name then
-		if type(data.item ) == "number" then
-			name:SetText(getItemLinkFromItemId(data.item))
-		else
-			name:SetText(data.item)
+	for craft = 1, 7 do
+		local rowCraftControl = GetControl(control, "Craft"..craft)
+		if rowCraftControl then
+			local BG = GetControl(rowCraftControl, "BG")
+			local name = GetControl(rowCraftControl, "Name")
+			local amount = GetControl(rowCraftControl, "Amount")
+			if name then
+				name:SetColor(0.4627,0.73725,0.7647)
+				if data[craft] == nil then
+					name:SetText("")
+				elseif type(data[craft].item ) == "number" then
+					if not data[craft].item then
+						d(data[craft].amount)
+					end
+					name:SetText(getItemLinkFromItemId(data[craft].item))
+				else
+					if not data[craft].item then
+						d(data[craft])
+					end
+					name:SetText(data[craft].item)
+				end
+			end
+			if amount then
+				if data[craft] == nil then
+					amount:SetText("")
+				else
+					amount:SetText(data[craft].amount or "")
+				end
+			end
+			if BG then
+				BG:SetAnchorFill(control)
+			end	
 		end
 	end
-	if amount then
-		amount:SetText(data.amount or "")
-	end
-	if BC then
-		BG:SetAnchorFill(control)
-		BG:SetCenterColor(1, 0.5, 0.5, 0.2)
-		BG:SetEdgeColor(0,0,0,0)
-	end
 	ZO_SortFilterList.SetupRow(self, control, data)
-	BG:SetCenterColor(1,0,0)
-	if  data.header then
-		name:SetText(GetCraftingSkillName(data.craft).. " Rewards")
-		name:SetColor(0,1,0)
-	end
+	-- local BG = GetControl(control, "BG")
+	-- local name = GetControl(control, "Name")
+	-- local amount = GetControl(control, "Amount")
+	
+
+	-- if BG then
+	-- 	BG:SetAnchorFill(control)
+	-- 	BG:SetCenterColor(1, 0.5, 0.5, 0.2)
+	-- 	BG:SetEdgeColor(0,0,0,0)
+	-- end
+	
+	-- BG:SetCenterColor(1,0,0)
+	-- if  data.header then
+	-- 	name:SetText(GetCraftingSkillName(data.craft).. " Rewards")
+	-- 	name:SetColor(0,1,0)
+	-- end
 end
+
+local rewardNameMaps = 
+{
+	["master"] = "Sealed Writs",
+	["intricate"] = "Intricate Gear",
+	["material"] = "Low level mats",
+	["survey"] = "Surveys",
+	["soulGem"] = "Soul Gems",
+	["glyph"] = "Decon Glyphs",
+	["repair"] = "Repair Kits",
+	["ornate"] = "Ornate Gear"
+}
 WritCreater.quality = {}
 
 for i = 1, 5 do
@@ -76,28 +113,59 @@ function RewardsScroll:BuildMasterList()
 
 	for craft, craftRewards in pairs(self.data) do
 		local craftHeader = {["craft"] = craft ,["header"]=true}
-		table.insert(self.masterList, 
-				craftHeader
-			)
+		local craftMasterList = {}
+		-- table.insert(self.masterList, 
+		-- 		craftHeader
+		-- 	)
+		local i = 1
 		for reward, amount in pairs(craftRewards) do
+
 			if reward=="num" then
-				craftHeader.amount=amount.." done"
+				craftHeader.amount=amount.." Writs Done"
 			elseif reward=="fragment" then
+				local name
+				if craft == CRAFTING_TYPE_PROVISIONING then
+					name = "Psijic Fragment"
+				else
+					name = "Glass Fragment"
+				end
+				self.masterList[i] = self.masterList[i] or {}
+				self.masterList[i][craft]= {["craft"] = craft ,["item"]=name, ["amount"]=amount}
+				i = i+1
 			else
+				
 				if type(amount)=="table" then
 					for qualifier, qualifierAmount in pairs(amount) do
 						if qualifierAmount>0 then
-							table.insert(self.masterList, {["craft"] = craft ,["item"]=reward,["qualifier"]=qualifier, ["amount"]=qualifierAmount})
+							self.masterList[i] = self.masterList[i] or {}
+							local name
+
+							if qualifier == "blue" then
+								name = "Blue Recipes"
+							elseif qualifier == "purple" then
+								name = "Purple Recipes"
+							elseif qualifier == "green" then
+								name = "Green Recipes"
+							end
+							self.masterList[i][craft] =  {["craft"] = craft ,["item"]=name,["qualifier"]=qualifier, ["amount"]=qualifierAmount}
+							i = i+1
 						end
 					end
 				else
-					table.insert(self.masterList, 
-						{["craft"] = craft ,["item"]=reward, ["amount"]=amount}
-					)
+					self.masterList[i] = self.masterList[i] or {}
+					self.masterList[i][craft]= {["craft"] = craft ,["item"]=rewardNameMaps[reward] or reward, ["amount"]=amount}
+					i = i+1
 				end
 			end
 		end
-
+		local header = GetControl(DolgubonsLazyWritStatsWindowBackdropCraftHeader,"Craft"..craft)
+		local  headerName= GetControl(header,"Name")
+		headerName:SetText(GetCraftingSkillName(craftHeader.craft).. " Rewards")
+		headerName:SetColor(0,1,0)
+		local headerAmount = GetControl(header, "Amount")
+		headerAmount:SetText(craftHeader.amount)
+		headerAmount:ClearAnchors()
+		headerAmount:SetAnchor(TOP, headerName, BOTTOM, 80, 0)
 	end
 
 end
@@ -125,5 +193,5 @@ end
 updateList = function () 
 	WritCreater.rewardsScrollManager:RefreshData()
 	DolgubonsLazyWritStatsWindowBackdropWritCounter:SetText("Writs Done: ".. WritCreater.savedVarsAccountWide.total)
- end
+end
 WritCreater.updateList = updateList
